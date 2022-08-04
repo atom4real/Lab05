a9726ff806ac059ab2e92839f472da0fd59e56bb
atomfaster@gmail.com <atomfaster@gmail.com>
2022-08-04 21:54:41 +0700

    1.6

--- src/views/event/EventEditView.vue
+++ src/views/event/EventEditView.vue
@@ -1,9 +1,23 @@
 <template>
   <p>Edit the event here</p>
+  <button @click="details">Details here!</button>
 </template>
 
 <script>
 export default {
-  props: ['id', 'event']
+  props: ['id', 'event'],
+  inject: ['GStore'],
+  methods: {
+    details() {
+      this.GStore.flashMessage = 'You are moved to ' + this.event.title
+      setTimeout(() => {
+        this.GStore.flashMessage = ''
+      }, 3000)
+      this.$router.push({
+        name: 'EventDetails',
+        params: { id: this.event.id }
+      })
+    }
+  }
 }
 </script>
